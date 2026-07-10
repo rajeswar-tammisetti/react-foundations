b2c3ae39f2e0460ffd941a020438394ec1f806d2
rajeswar-tammisetti <rajeswartammisetti@gmail.com>
2026-07-10 21:57:46 +0530

    Custom components challenge Part-2

--- Static-pages/src/main.jsx
+++ Static-pages/src/main.jsx
@@ -1,35 +1,39 @@
-import { createRoot } from "react-dom/client"
-const root = createRoot(document.getElementById("root"))
-import './index.css'
+import {createRoot} from "react-dom/client"
+import "./index.css"
 
 /**
- * Challenge (part 1):
- * Create a custom "Page" component
- * 
- * It should return an ordered list with the reasons why you're
- * excited to be learning React :)
- * 
- * Render the Page component.
+Challenge: 
+
+Part 2: 
+- Add a `<header>` element with an `<img />` element with the image of the 
+  React logo inside (src="react-logo.png") and make sure to set the 
+  width to something more manageable so it doesn't take up the whole screen.
+  Also, as always, you should include some alt text on the image.
+- Add an `<h1>` with some text describing the page. (E.g. "Reasons
+  I'm excited to learn React"). Place it above the ordered list, then wrap
+  the `<h1>` and `<ol>` inside a `<main>` element to keep our semantic
+  structure flowing well.
+- Add a `<footer>` after the list that says: 
+    "© 20xx <last name here> development. All rights reserved."
  */
 
+const root=createRoot(document.getElementById("root"));
 root.render(
-    <main>
-        {Page()}
-        <Page />
-    </main>  
+    <Page></Page>
 )
-
 function Page(){
     return (
-        <main>
-            <img src="/react-logo.png" width="40px" alt="React logo" />
-            <h1>Why I'm excited to learn React!</h1>
-            <ol>
-                <li>It's highly employable and used by top companies.</li>
-                <li>The component-based architecture makes building UI clean and reusable.</li>
-                <li>It opens the door to building mobile apps with React Native later on.</li>
-            </ol>
-    </main>
+        <div>
+            <header><img src="react-logo.png" alt="react-logo" width="40px"></img></header>
+            <main>
+                <h1>Why I'm excited to learn React!</h1>
+                <ol>
+                    <li>It's highly employable and used by top companies.</li>
+                    <li>The component-based architecture makes building UI clean and reusable.</li>
+                    <li>It opens the door to building mobile apps with React Native later on.</li>
+                </ol>
+            </main>
+            <footer><small>© 2026 GRR development. All rights reserved.</small></footer>
+        </div>
     )
 }
-
