5b838b03a17d6ab6b8b9ff3b0bac13b12766dd40
rajeswar-tammisetti <rajeswartammisetti@gmail.com>
2026-07-21 19:20:34 +0530

    Recipe Craft reading form

--- Static-pages/src/Projects/RecipeCraft/components/Main.jsx
+++ Static-pages/src/Projects/RecipeCraft/components/Main.jsx
@@ -1,13 +1,34 @@
 export default function Main() {
     const ingredients = ["Chicken", "Oregano", "Tomatoes"]
-    const ingredientsList =ingredients.map(ingredient =>
-        (
-            <li>{ingredient}</li>
-        ))
+    const ingredientsList = ingredients.map(ingredient =>
+    (
+        <li>{ingredient}</li>
+    ))
+
+    /**
+     * Challenge:
+     * Add an `onSubmit` event listener on the form. Have the function
+     * simply console.log("Form submitted!") for now
+     
+     * Challenge:
+     * Add the new ingredient to the array of ingredients. Also, add a 
+     * console.log(ingredients) after adding the ingredient, because 
+     * **warning**: you aren't going to see the page update!
+     * 
+     * Hint: this is a one-liner solution, so don't overthink it 😅
+     */
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        console.log(newIngredient)
+    }
+
     return (
         <main>
-            <form className="add-ingredient-form">
-                <input type="text" placeholder="e.g. oregan" aria-label="Add ingredient" />
+            <form className="add-ingredient-form" onSubmit={handleSubmit}>
+                <input type="text" placeholder="e.g. oregan" aria-label="Add ingredient" name="ingredient" />
                 <button>Add ingredient</button>
             </form>
             <ul>
